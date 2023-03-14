a19291d778967f13db93a6621f3164c88f72050d
Michinosuke <michinosuke.info@gmail.com>
2023-03-14 11:05:24 +0900

    update header link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ export const Header = () => (
   <div className="w-full bg-white shadow-lg px-5 py-3 flex gap-2 items-center">
     <img src="/assets/icon.svg" className="h-5 w-5 mt-[1px]" />
     <h1 className="text-xl">
-      <a href="/course">
+      <a href="/">
         {/* <span className="font-bold text-main">exam</span>
         <span className="text-gray-500">pack</span> */}
         <span className="text-gray-500">exam</span>
