28a28f6cd0acf9437d46e71feb4aa322b1c34223
michinosuke <michiya.318.3738@gmail.com>
2023-03-18 00:10:23 +0900

    a

--- src/libs/remote.ts
+++ src/libs/remote.ts
@@ -1,16 +1,21 @@
-import axios from "axios";
-import type { Exam } from "../interfaces/exam";
 import { CREATE_EXAM_FUNCTION_URL, GET_EXAM_FUNCTION_URL } from "./constants";
+
+import type { Exam } from "../interfaces/exam";
+import axios from "axios";
 
 const save = async (
   exam: Exam,
   examId?: string | undefined | null
 ): Promise<{ examId: string; examUrl: string }> => {
-  const body: { exam: any; examId?: string } = {
+  const body: { exam: Exam; examId?: string } = {
     exam,
   };
   if (examId) body.examId = examId;
-  const { data } = await axios.post(CREATE_EXAM_FUNCTION_URL, body);
+  const { data } = await axios.post(CREATE_EXAM_FUNCTION_URL, body, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   return {
     examId: data.examId,
     examUrl: `https://exam.blue/exam?source=${GET_EXAM_FUNCTION_URL}?examId=${data.examId}`,
